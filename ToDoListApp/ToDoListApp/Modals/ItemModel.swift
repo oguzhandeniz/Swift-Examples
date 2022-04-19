01a63c229e7f1205d8f8ca321cd830762f8ae595
oguzhandeniz <oguzhandeniz2512@gmail.com>
2022-04-19 14:11:13 +0300

    Create ItemModel.swift

---
+++ ToDoListApp/ToDoListApp/Modals/ItemModel.swift
@@ -0,0 +1,25 @@
+//
+//  ItemModel.swift
+//  ToDoListApp
+//
+//  Created by Oğuzhan Deniz on 18.04.2022.
+//
+
+import Foundation
+
+struct ItemModel: Identifiable, Codable {
+    let id: String
+    let title: String
+    let isCompleted: Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
+    
+}
