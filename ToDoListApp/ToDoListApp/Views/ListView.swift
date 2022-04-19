7d34f77566c0724ee13e5bc91c0b268a9b674c2a
oguzhandeniz <oguzhandeniz2512@gmail.com>
2022-04-19 14:10:12 +0300

    Create ListView.swift

---
+++ ToDoListApp/ToDoListApp/Views/ListView.swift
@@ -0,0 +1,52 @@
+//
+//  ListView.swift
+//  ToDoListApp
+//
+//  Created by Oğuzhan Deniz on 18.04.2022.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @EnvironmentObject var listViewModel: ListViewModel
+    
+    var body: some View {
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
+                    }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
+            }
+        }
+        .navigationTitle("To Do List 📝")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing:
+                NavigationLink("Add", destination: AddView())
+            )
+    }
+
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+        .environmentObject(ListViewModel())
+    }
+}
