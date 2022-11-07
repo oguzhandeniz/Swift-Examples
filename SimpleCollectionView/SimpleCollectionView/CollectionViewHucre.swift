992c0bf928ad0215cc4cf9edf9a70760a7fb0653
oguzhandeniz <oguzhandeniz2512@gmail.com>
2022-11-07 14:00:36 +0300

    Create CollectionViewHucre.swift

---
+++ SimpleCollectionView/SimpleCollectionView/CollectionViewHucre.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewHucre.swift
+//  SimpleCollectionView
+//
+//  Created by Oğuzhan Deniz on 3.11.2022.
+//
+
+import UIKit
+
+class CollectionViewHucre: UICollectionViewCell {
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+    
+}
