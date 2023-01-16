fb728fc9e9d9fcb5fd668c260ca6094e5e980fa7
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-01-16 20:25:29 +0300

    Create NotHucreTableViewCell.swift

---
+++ NotUygulamasiFireBase/NotUygulamasi/NotHucreTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  NotHucreTableViewCell.swift
+//  NotUygulamasi
+//
+//  Created by Oğuzhan Deniz on 10.12.2022.
+//
+
+import UIKit
+
+class NotHucreTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelDersAdi: UILabel!
+    
+    @IBOutlet weak var labelNot1: UILabel!
+    
+    @IBOutlet weak var labelNot2: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
