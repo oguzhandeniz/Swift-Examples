2cecee9e80b8397389cc33d626bfc67bef593169
oguzhandeniz <oguzhandeniz2512@gmail.com>
2023-03-20 13:57:34 +0300

    Viper Pattern with SQLite

---
+++ KisilerAppViperPattern(SQLite)/KisilerUygulamasi/anasayfa-module/router/AnasayfaRouter.swift
@@ -0,0 +1,21 @@
+//
+//  AnasayfaRouter.swift
+//  KisilerUygulamasi
+//
+//  Created by Oğuzhan Deniz on 6.03.2023.
+//
+
+import Foundation
+
+class AnasayfaRouter : PresenterToRouterAnasayfaProtocol {
+    static func createModule(ref: AnasayfaVC) {
+        let presenter = AnasayfaPresenter()
+        
+        ref.anasayfaPresenterNesnesi = presenter
+        
+        ref.anasayfaPresenterNesnesi?.anasayfaInteractor = AnasayfaInteractor()
+        ref.anasayfaPresenterNesnesi?.anasayfaView = ref
+        
+        ref.anasayfaPresenterNesnesi?.anasayfaInteractor?.anasayfaPresenter = presenter
+    }
+}
---
+++ KisilerAppViperPattern(SQLite)/KisilerUygulamasi/anasayfa-module/tableview/TableViewHucre.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewHucre.swift
+//  KisilerUygulamasi
+//
+//  Created by Oğuzhan Deniz on 02.03.2023.
+//
+
+import UIKit
+
+class TableViewHucre: UITableViewCell {
+
+    @IBOutlet weak var kisiBilgiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
